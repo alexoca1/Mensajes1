712ca65f7b5317a2f1529431213caf9891820c54
alexoca1 <alexanderocaher@gmail.com>
2023-03-09 14:04:34 +0100

    primer comit Mensajes

--- src/main/java/com/mycompany/mensajes/Main.java
+++ src/main/java/com/mycompany/mensajes/Main.java
@@ -1,16 +1,32 @@
-
 package com.mycompany.mensajes;
 
 import java.sql.SQLException;
-
+import java.util.*;
 
 public class Main {
-        
+
         public static void main(String[] args) throws SQLException, ClassNotFoundException {
-                
-                Conexion.getConexion();
-                System.out.println("conexion exitosa");
+                System.out.println("RRRRRRRRRRRRRRRRRRRRRR");
+                //Conexion.getConexion();
+                // System.out.println("conexion exitosa");
+                MensajeDao mensajeDao = new MensajeDao();
+
+                // Mensaje msm= new Mensaje("hola desde Main", "Main");
+                //  int registro = mensajeDao.insertar(msm);
+                //   System.out.println("se inserto "+registro+" registro");
+                //Mensaje msm = new Mensaje(1,"mensaje desde java web", "java web");
+                // int registro = mensajeDao.editar(msm);
+                // System.out.println("se edito " + registro + " registro");
+                Mensaje msm = new Mensaje(6);
+                int registro = mensajeDao.eliminar(msm);
+                System.out.println("se elimino " + registro + " registro");
+
+                List<Mensaje> mensajes = mensajeDao.seleccionar();
+
+                for (Mensaje mensaje : mensajes) {
+                        System.out.println(mensaje);
+                }
+
         }
-        
-        
+
 }
--- src/main/java/com/mycompany/mensajes/MensajeDao.java
+++ src/main/java/com/mycompany/mensajes/MensajeDao.java
@@ -1,23 +1,132 @@
 
 package com.mycompany.mensajes;
-import static com.
+import static com.mycompany.mensajes.Conexion.*;
+//import com.sun.org.apache.bcel.internal.generic.InstructionConst;
+
 import java.sql.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class MensajeDao {
         
-        private Connection connection=null;
+        private Connection conn=null;
         private PreparedStatement ps= null;
         private ResultSet rs=null;
         private Mensaje mensaje;
-        
-    public List<Mensaje> seleccionar(){
+                     
+   
+    public List<Mensaje> seleccionar() throws SQLException{
+                            
+
         String sql = "SELECT * FROM mensajes";
         List<Mensaje> mensajes = new ArrayList<>();
         
-        this.conn = 
+                try {
+                        this.conn = getConexion();
+                        this.ps = this.conn.prepareStatement(sql);
+                        this.rs =this.ps.executeQuery();
+                        
+                        while(this.rs.next()){
+                        int id = this.rs.getInt("id_mensaje");
+                        String msm = this.rs.getString("mensaje");
+                        String autor= this.rs.getString("autor");
+                        String fechar = this.rs.getString("fecha");
+                        
+                        this.mensaje = new Mensaje(id,msm,autor,fechar);
+                        
+                        mensajes.add(this.mensaje);
+                        
+                        }
+                } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace(System.out);
+                }finally{
+                        cerrar(this.rs);
+                        cerrar(this.ps);
+                        cerrar(this.conn);
+                }
+                return mensajes;
+            
+    }
+    
+       public int insertar(Mensaje mensaje) throws SQLException{
+                             
+
+        String sql = "INSERT INTO mensajes (mensaje, autor, fecha) VALUES (?,?,CURRENT_TIME())";
+        
+                int registros = 0;
+                try {
+                        this.conn = getConexion();
+                        this.ps = this.conn.prepareStatement(sql);
+                        
+                        this.ps.setString(1, mensaje.getMensaje());
+                        this.ps.setString(2, mensaje.getAutor());
+                        registros = this.ps.executeUpdate();
+                      
+                        
+                } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace(System.out);
+                }finally{
+                        
+                        cerrar(this.ps);
+                        cerrar(this.conn);
+                }
+                return registros;
+            
+    }
+        public int editar(Mensaje mensaje) throws SQLException{
+                             
+
+        String sql = "UPDATE mensajes SET mensaje=?,autor=? WHERE id_mensaje=?";
+        
+                int registros = 0;
+                try {
+                        
+                        this.conn = getConexion();
+                        this.ps = this.conn.prepareStatement(sql);
+                        
+                        
+                        this.ps.setString(1, mensaje.getMensaje());
+                        this.ps.setString(2, mensaje.getAutor());
+                        this.ps.setInt(3, mensaje.getId());
+                        registros = this.ps.executeUpdate();
+                                                
+                } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace(System.out);
+                }finally{
+                        
+                        cerrar(this.ps);
+                        cerrar(this.conn);
+                }
+                return registros;
+            
+    }
+        public int eliminar(Mensaje mensaje) throws SQLException{
+                             
+
+        String sql = "DELETE FROM mensajes WHERE id_mensaje=?";
+        
+                int registros = 0;
+                try {
+                        
+                        this.conn = getConexion();
+                        this.ps = this.conn.prepareStatement(sql);
+                        
+                        
+                       
+                        this.ps.setInt(1, mensaje.getId());
+                        registros = this.ps.executeUpdate();
+                                                
+                } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace(System.out);
+                }finally{
+                        
+                        cerrar(this.ps);
+                        cerrar(this.conn);
+                }
+                return registros;
             
     }
                 
-        }
+}  
         
-}
+
